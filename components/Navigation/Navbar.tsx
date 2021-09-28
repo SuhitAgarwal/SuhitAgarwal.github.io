ef876053fc0007024c7adc5268ad9cd9f93a91f5
Suhit Agarwal <suhitaga@usc.edu>
2021-09-28 15:17:27 -0700

    updates, updates

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import Link from 'next/link'
-import { FiGithub, FiLinkedin, FiBehance } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaBehance } from 'react-icons/fa'
 import { motion } from 'framer-motion'
 import { FaReadme } from 'react-icons/fa'
 
